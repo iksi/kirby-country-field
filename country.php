cad267b4244f82909dbdb663d956bf064b200fb8
iksi <jur@iksi.cc>
2014-12-28 18:50:07 +0100

    moved country.json data file

--- country.php
+++ country.php
@@ -3,7 +3,7 @@
 class CountryField extends SelectField {
 
     public function options() {
-        $json = file_get_contents(__DIR__ . '/country.json');
+        $json = file_get_contents(__DIR__ . 'assets/data/country.json');
 
         $options = json_decode($json, TRUE);
 
